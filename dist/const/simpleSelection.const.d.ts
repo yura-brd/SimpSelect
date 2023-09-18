c7770f0afa0666e4937b7cb2534e295f3087dc56
YuriiK <yura.brd@gmail.com>
2023-09-18 22:30:41 +0300

    add types

---
+++ dist/const/simpleSelection.const.d.ts
@@ -0,0 +1,6 @@
+import { ISimpleSelectLocale, ISimpleSelectOptions } from '../types/simpleSelect.types';
+export declare const simpleSelectLocale: ISimpleSelectLocale;
+export declare const simpleSelectionOptions: ISimpleSelectOptions;
+export declare const nameSelect = "SimSel";
+export declare const nameMark = "simple-select-init";
+export declare const initClass = "SimpleSel";
